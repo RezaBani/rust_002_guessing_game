ea8f9562af5d004507d4a8e32bd5827ebc1060db
RezaBanivakil <rezabaniv@gmail.com>
2025-03-29 10:54:06 +0330

    implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    
+    let secret_number = 5;
+    
+    loop {
+        println!("Guess the number!");
+        let mut guess = String::new();
+        std::io::stdin().read_line(&mut guess).expect("Failed To read the input");
+
+        let guess:i32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Guess too small"),
+            std::cmp::Ordering::Equal => {
+                println!("Guess correct"); 
+                break;
+            }
+            std::cmp::Ordering::Greater => println!("Guess too large"),
+        }
+    }
 }
