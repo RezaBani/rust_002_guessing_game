81963498f6547ae6d4f6f1335fb6223ed5c8d011
RezaBanivakil <rezabaniv@gmail.com>
2025-04-04 18:46:32 +0330

    0.1.0
    generating random number from zig, quiting with q

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,55 @@
-fn main() {
-    
-    let secret_number = 5;
-    
-    loop {
-        println!("Guess the number!");
-        let mut guess = String::new();
-        std::io::stdin().read_line(&mut guess).expect("Failed To read the input");
+use std::{cmp, env, fmt::Debug, io, process::Command, str::FromStr};
 
-        let guess:i32 = match guess.trim().parse(){
+fn main() {
+    type NumberType = u8;
+    let secret_number = generate_random_number::<NumberType>();
+
+    println!("Guess the number! (Type q to quit eartly)");
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed To read the input");
+
+        let guess: NumberType = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                if guess.trim().to_lowercase() == "q" {
+                    break;
+                }
+                continue;
+            }
         };
 
         match guess.cmp(&secret_number) {
-            std::cmp::Ordering::Less => println!("Guess too small"),
-            std::cmp::Ordering::Equal => {
-                println!("Guess correct"); 
+            cmp::Ordering::Less => println!("Guess too small"),
+            cmp::Ordering::Equal => {
+                println!("Guess correct");
                 break;
             }
-            std::cmp::Ordering::Greater => println!("Guess too large"),
+            cmp::Ordering::Greater => println!("Guess too large"),
         }
     }
+}
+
+fn generate_random_number<T: std::str::FromStr>() -> T
+where
+    <T as FromStr>::Err: Debug,
+{
+    let rust_cwd = env::current_dir().unwrap();
+    env::set_current_dir("../../Zig/zig_004_random_number_generator/").expect("Change cwd failed");
+    let mut zig = Command::new("zig");
+    let output = zig
+        .args(vec![
+            "build",
+            "run",
+            "--",
+            format!("{}", std::any::type_name::<T>()).as_str(),
+        ])
+        .output()
+        .unwrap();
+    let number = String::from_utf8(output.stderr).unwrap();
+    let number = number.trim();
+    env::set_current_dir(rust_cwd).unwrap();
+    number.parse().unwrap()
 }
